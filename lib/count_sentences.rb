dd4298a7da954d6e8edf72e1c9f207503093cf69
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 14:44:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    self.scan(/\w[.?!]/).size
   end
 end
